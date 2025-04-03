ce141eed00627c997a21b9ec7bef5c97b25b5886
ganadipa <nyomanganadipa@gmail.com>
2025-04-03 21:46:39 +0700

    feat (navigation): add navigation after splash screen

---
+++ app/src/main/java/com/example/adbpurrytify/ui/navigation/AppNavigation.kt
@@ -0,0 +1,85 @@
+package com.example.adbpurrytify.ui.navigation
+
+import android.util.Log
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MenuDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.composable
+
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.adbpurrytify.HomePage
+import com.example.adbpurrytify.ui.screens.LoginScreen
+import com.example.adbpurrytify.ui.screens.SplashScreen
+
+@Composable
+fun AppNavigation(navController: NavHostController = rememberNavController()) {
+
+    // Get the main screens that will be shown at the bottom navbar
+    val navigationItems = listOf(
+        NavigationItem.Home,
+        NavigationItem.Library,
+        NavigationItem.Profile
+    )
+
+    // For each of the navigation item, then get the route
+    val mainRoutes = navigationItems.map { item -> item.route }
+
+
+    // Get current route to determine if we should show bottom navigation
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    /**
+     * Should we show the bottom bar?
+     *
+     * Note from @ganadipa: I think this will be useful for some pages that don't need
+     * a navbar, perhaps they only need a back button, what do u think?
+     */
+    val showBottomBar = currentRoute in mainRoutes
+
+    Scaffold (
+        bottomBar = {
+            if (showBottomBar) {
+                BottomNavigationBar(
+                    navController = navController,
+                    items = navigationItems
+                )
+            }
+        }
+    ) { paddingValues ->
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Splash.route,
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            composable(Screen.Splash.route) {
+                SplashScreen(navController)
+            }
+
+            composable(Screen.Login.route) {
+                LoginScreen(navController)
+            }
+
+            composable(Screen.Home.route) {
+                HomePage()
+            }
+
+            composable(Screen.Library.route) {
+                // TO DO
+            }
+
+            composable(Screen.Profile.route) {
+                // TO DO
+            }
+
+        }
+    }
+}
+
--- app/src/main/java/com/example/adbpurrytify/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/example/adbpurrytify/ui/screens/SplashScreen.kt
@@ -1,24 +1,64 @@
 package com.example.adbpurrytify.ui.screens
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavController
+import com.example.adbpurrytify.R
+import com.example.adbpurrytify.ui.navigation.Screen
+import com.example.adbpurrytify.ui.theme.BLACK_BACKGROUND
 import kotlinx.coroutines.delay
 
 @Composable
-fun SplashScreen(onNavigateToLogin: () -> Unit) {
-    // Auto-navigate to login after delay
-    LaunchedEffect(key1 = true) {
-        delay(2000) // 2 seconds delay
-        onNavigateToLogin()
+fun SplashScreen(navController: NavController) {
+    val scale = remember {
+        Animatable(1f)
     }
 
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        // Your logo or splash content here
-        Text(text = "ADBPurrytify")
+    // To do: Implement the isLoggedIn functionality
+    val isLoggedIn = true
+
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 1.3f,
+            animationSpec = tween(
+                durationMillis = 500,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
+                }
+            )
+        )
+        delay(3000L)
+        if (isLoggedIn) {
+            navController.navigate(Screen.Home.route) {
+                popUpTo(Screen.Splash.route) { inclusive = true }
+            }
+        } else {
+            navController.navigate(Screen.Login.route) {
+                popUpTo(Screen.Splash.route) { inclusive = true }
+            }
+        }
+    }
+
+    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize().background(
+        BLACK_BACKGROUND
+    )) {
+        Image(
+            painter = painterResource(id = R.drawable.logo_purrytify),
+            contentDescription = "Logo",
+            modifier = Modifier.scale(scale.value)
+        )
     }
 }
