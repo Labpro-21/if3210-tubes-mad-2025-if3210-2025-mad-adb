8394a6498433905b32dbe80fc19f6fb82f152774
ganadipa <nyomanganadipa@gmail.com>
2025-04-09 21:49:09 +0700

    nit: fix build error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,7 +76,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.security.crypto.ktx)
+    implementation("androidx.security:security-crypto-ktx:1.1.0-alpha06")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
