aaaa645385ada00cba651c4eea9c7c7d673f6a95
CrystalNoob <69108822+CrystalNoob@users.noreply.github.com>
2025-04-09 16:18:38 +0700

    feat: Add image picker and improve UX

--- app/src/main/java/com/example/adbpurrytify/ui/screens/AddSong.kt
+++ app/src/main/java/com/example/adbpurrytify/ui/screens/AddSong.kt
@@ -25,10 +25,12 @@ import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -36,22 +38,41 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil3.compose.rememberAsyncImagePainter
 import com.example.adbpurrytify.R
 import com.example.adbpurrytify.ui.theme.ADBPurrytifyTheme
+import kotlinx.coroutines.launch
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddSong() {
     val padding = 32.dp
-    var uploadPhotoPainter = painterResource(R.drawable.upload_file)
-    var titleText by remember { mutableStateOf("") }
-    var artistText by remember { mutableStateOf("") }
-    var photoSelected = false
+
+    // ModalBottomSheet params
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+    var showBottomSheet by rememberSaveable { mutableStateOf(false) }
+
+    // Remember user input, even when the screen is rotated :(
+    var titleText by rememberSaveable { mutableStateOf("") }
+    var artistText by rememberSaveable { mutableStateOf("") }
+    var photoUri by rememberSaveable { mutableStateOf<android.net.Uri?>(null) }
+    var fileUri by rememberSaveable { mutableStateOf<android.net.Uri?>(null) }
+
+    val pickMedia = rememberLauncherForActivityResult(PickVisualMedia()) { uri ->
+        photoUri = uri
+
+        // Uncomment for DEBUGGING only, after enabling the below code, kindly check logcat
+        // Log.d("PhotoUri", "Photo URI: $photoUri")
+    }
 
     ADBPurrytifyTheme {
         Surface {
-            ModalBottomSheet(onDismissRequest = {}) {
+            ModalBottomSheet(
+                onDismissRequest = { showBottomSheet = false },
+                sheetState = sheetState
+            ) {
                 Column(
                     modifier = Modifier
                         .fillMaxSize()
@@ -70,11 +91,15 @@ fun AddSong() {
                                 .padding(start = padding, top = padding, end = padding)
                         ) {
                             Image(
-                                painter = uploadPhotoPainter,
+                                painter = if(photoUri == null) painterResource(R.drawable.upload_file) else rememberAsyncImagePainter(photoUri),
                                 contentDescription = "Upload Photo",
                                 modifier = Modifier
                                     .fillMaxSize()
-                                    .clickable(true, onClick = {})
+                                    .clickable(true,
+                                        onClick = {
+                                            pickMedia.launch(PickVisualMediaRequest(PickVisualMedia.ImageOnly))
+                                        }
+                                    )
                             )
                         }
                         Box(
@@ -153,7 +178,13 @@ fun AddSong() {
                         horizontalArrangement = Arrangement.Center
                     ) {
                         FilledTonalButton(
-                            onClick = {},
+                            onClick = {
+                                scope.launch { sheetState.hide() }.invokeOnCompletion {
+                                    if (!sheetState.isVisible) {
+                                        showBottomSheet = false
+                                    }
+                                }
+                            },
                             modifier = Modifier
                                 .fillMaxHeight()
                                 .fillMaxWidth(1/2f)
@@ -167,7 +198,7 @@ fun AddSong() {
                                 .fillMaxHeight()
                                 .fillMaxWidth(1f)
                                 .padding(horizontal = padding * 1/4f),
-                            enabled = titleText.isNotEmpty() && artistText.isNotEmpty() && photoSelected
+                            enabled = titleText.isNotEmpty() && artistText.isNotEmpty() && (photoUri != null)
                         ) {
                             Text("Save")
                         }
