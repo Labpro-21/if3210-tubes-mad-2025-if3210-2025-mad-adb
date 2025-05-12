1d565a9a6070121912f2fcacde55326bec31c81a
CrystalNoob <69108822+CrystalNoob@users.noreply.github.com>
2025-05-12 16:52:41 +0700

    chore: Remove unused files

--- app/src/main/java/com/example/adbpurrytify/data/model/Song.kt
+++
@@ -1,9 +0,0 @@
-package com.example.adbpurrytify.data.model
-
-data class Song(
-    val id: Int = -1,
-    val title: String,
-    val author: String,
-    val coverUrl: String,
-    val audioUrl: String
-)
