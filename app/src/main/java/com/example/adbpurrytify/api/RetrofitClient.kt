3c143388ec34e4edcf69b321ca91587d7f418d55
CrystalNoob <69108822+CrystalNoob@users.noreply.github.com>
2025-05-17 14:32:48 +0700

    feat: Add interceptor to okHttpClient

--- app/src/main/java/com/example/adbpurrytify/api/RetrofitClient.kt
+++ app/src/main/java/com/example/adbpurrytify/api/RetrofitClient.kt
@@ -2,6 +2,7 @@
 package com.example.adbpurrytify.api
 
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
@@ -10,10 +11,14 @@ object RetrofitClient {
 
     private const val BASE_URL = "http://34.101.226.132:3000/"
 
+    val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
     private val okHttpClient = OkHttpClient.Builder()
         .connectTimeout(30, TimeUnit.SECONDS)
         .readTimeout(30, TimeUnit.SECONDS)
-         .addInterceptor(AuthInterceptor())
+        .addInterceptor(AuthInterceptor())
+        // Add interceptor to check http response body
+        .addInterceptor(interceptor)
         .build()
 
     val instance: ApiService by lazy {
