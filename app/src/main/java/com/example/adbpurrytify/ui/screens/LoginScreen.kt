9a1d8a3276b181921deae91a8daf07b670e20908
CrystalNoob <69108822+CrystalNoob@users.noreply.github.com>
2025-05-25 14:30:54 +0700

    fix: Make the LoginScreen scrollable

--- app/src/main/java/com/example/adbpurrytify/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/adbpurrytify/ui/screens/LoginScreen.kt
@@ -11,8 +11,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
@@ -189,7 +191,8 @@ fun LoginScreen(
                 .fillMaxWidth()
                 .align(Alignment.BottomCenter)
                 .padding(horizontal = 32.dp)
-                .padding(bottom = 48.dp),
+                .padding(bottom = 48.dp)
+                .verticalScroll(rememberScrollState()), // How nice, to have this thing existed
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             PurritifyLogo(
